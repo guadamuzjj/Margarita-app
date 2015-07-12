0f6dfd7f3cb55e2513eb9f78273cde06505679ab
Oscar Cortez <om.cortez.2010@gmail.com>
2015-07-12 16:08:53 -0600

    Create New Activity
    Launch new activities on button click

--- app/src/main/java/com/stormcoders/margarita_app/ExerciseActivity.java
+++ app/src/main/java/com/stormcoders/margarita_app/ExerciseActivity.java
@@ -4,23 +4,21 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.content.Intent;
 
 
-public class MainActivity extends ActionBarActivity {
+public class ExerciseActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_exercise);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_exercise, menu);
         return true;
     }
 
@@ -38,23 +36,4 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
-
-    public void story (View view)
-    {
-        Intent intent = new Intent(MainActivity.this, StoryActivity.class);
-        startActivity(intent);
-    }
-
-    public void exercise(View view)
-    {
-        Intent intent = new Intent(MainActivity.this, ExerciseActivity.class);
-        startActivity(intent);
-    }
-
-    public void settings(View view)
-    {
-        Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
-        startActivity(intent);
-    }
-
 }
