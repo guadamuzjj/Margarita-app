ffbd26188c41d9fb5c4ab5f41c5f7739a499ee79
Jonathan Guadamuz <guadamuzjj@gmail.com>
2015-07-14 23:28:09 -0600

    Necessary changes for new exercise package.

--- app/src/main/java/com/stormcoders/margarita_app/MainActivity.java
+++ app/src/main/java/com/stormcoders/margarita_app/MainActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.content.Intent;
 import android.view.WindowManager;
 
+import com.stormcoders.margarita_app.exercise.ExerciseActivity;
 import com.stormcoders.margarita_app.story.StoryActivity;
 
 
