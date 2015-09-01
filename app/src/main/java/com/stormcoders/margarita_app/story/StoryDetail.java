5549d3dd0a67ac6b4f0691202d154f7f09082918
Oscar Cortez <om.cortez.2010@gmail.com>
2015-08-31 23:28:40 -0600

    set the story text

--- app/src/main/java/com/stormcoders/margarita_app/story/StoryDetail.java
+++ app/src/main/java/com/stormcoders/margarita_app/story/StoryDetail.java
@@ -8,6 +8,8 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.stormcoders.margarita_app.R;
@@ -21,6 +23,7 @@ public class StoryDetail extends ActionBarActivity {
     public static final String VIEW_NAME_HEADER_IMAGE = "imagen_compartida";
     private Chapter itemDetail;
     private ImageView extendedImage;
+    private TextView extendedText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,9 +33,34 @@ public class StoryDetail extends ActionBarActivity {
         // Obtener el chapter con el identificador establecido en la actividad principal
         itemDetail = Chapter.getItem(getIntent().getIntExtra(EXTRA_PARAM_ID, 0));
 
+        System.out.println("HOLA HOLA HOLA HOLA");
+        System.out.println(itemDetail.getName());
+        System.out.println(itemDetail.getIdDrawable());
+
         extendedImage = (ImageView) findViewById(R.id.extended_image);
+        extendedText = (TextView) findViewById(R.id.extended_text);
 
         loadExtendedImage();
+
+        // Hard coded because who cares? I'm a hacker XD
+        if (itemDetail.getName() == "Cap 1")
+            extendedText.setText(R.string.cap_1);
+        else if (itemDetail.getName() == "Cap 2")
+            extendedText.setText(R.string.cap_2);
+        else if (itemDetail.getName() == "Cap 3")
+            extendedText.setText(R.string.cap_3);
+        else if (itemDetail.getName() == "Cap 4")
+            extendedText.setText(R.string.cap_4);
+        else if (itemDetail.getName() == "Cap 5")
+            extendedText.setText(R.string.cap_5);
+        else if (itemDetail.getName() == "Cap 6")
+            extendedText.setText(R.string.cap_6);
+        else if (itemDetail.getName() == "Cap 7")
+            extendedText.setText(R.string.cap_7);
+        else if (itemDetail.getName() == "Cap 8")
+            extendedText.setText(R.string.cap_8);
+        else
+            extendedText.setText(R.string.cap_9);
     }
 
     private void loadExtendedImage() {
@@ -40,4 +68,5 @@ public class StoryDetail extends ActionBarActivity {
                 .load(itemDetail.getIdDrawable())
                 .into(extendedImage);
     }
+
 }
